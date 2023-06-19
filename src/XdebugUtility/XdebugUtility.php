9ff0ead4f83ed9ca1aaf700679440096c3fe3af7
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-06-19 12:07:59 -0400

    Add CSS styles and modify the enqueue so that the script requires jquery before it as dependency.

--- src/XdebugUtility/XdebugUtility.php
+++ src/XdebugUtility/XdebugUtility.php
@@ -6,11 +6,12 @@ class XdebugUtility
 {
 	public function __construct()
 	{
-		add_action('wp_enqueue_scripts', [$this, 'enqueue_script']);
+		add_action('wp_enqueue_scripts', [$this, 'enqueue_frontend_assets']);
 	}
 
-	public function enqueue_script()
+	public function enqueue_frontend_assets()
 	{
-		wp_enqueue_script('hsc-xdebug-utility', XDEBUG_UTILITY_PLUGIN_DIR_URL . 'assets/js/xdebug-utility.js', '1.0.0');
+		wp_enqueue_script('hsc-xdebug-utility-js', XDEBUG_UTILITY_PLUGIN_DIR_URL . 'assets/js/xdebug-utility.js', array('jquery'), '1.0.0', false);
+		wp_enqueue_style('hsc-xdebug-utility-css', XDEBUG_UTILITY_PLUGIN_DIR_URL . 'assets/css/xdebug-utility.css', '1.0.0');
 	}
 }
